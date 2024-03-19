ecdff7133aba0cbf892e80a0dc61863e6886e4b3
ldx027 <17301339186@163.com>
2024-03-20 07:20:17 +0800

    feat: 2_10

--- 程序设计训练/2_10.cpp
+++ 程序设计训练/2_10.cpp
@@ -5,16 +5,13 @@ using namespace std;
 
 struct Node
 {
-    int x, y;
-    int n = 0;
+    int x, y, n;
+    Node() : n(0) {}
 };
 
 int abs(int x) { return x > 0 ? x : -x; }
-
 bool cmp(const Node &a, const Node &b) { return a.n > b.n; }
-
 inline int byRoad(Node a, Node b) { return a.x + b.x + 2; }
-
 inline int byJmp(Node a, Node b) { return abs(a.x - b.x) + abs(a.y - b.y); }
 
 int main()
@@ -30,17 +27,20 @@ int main()
     {
         int M, N, K;
         cin >> M >> N >> K;
-        
+
         vector<Node> vec(N * M + 1);
-        for (int i = 0; i < M; i++) for (int j = 0; j < N; j++)
+        for (int i = 0; i < M; i++)
         {
-            vec[i * M + j].x = i;
-            vec[i * M + j].y = j;
-            cin >> vec[i * M + j].n;
+            for (int j = 0; j < N; j++)
+            {
+                vec[i * N + j].x = i;
+                vec[i * N + j].y = j;
+                cin >> vec[i * N + j].n;
+            }
         }
         sort(vec.begin(), vec.end(), cmp);
 
-        int time = vec[0].x + 1 + 1; 
+        int time = vec[0].x + 1 + 1;
         int total = 0;
 
         for (int i = 0; i < vec.size() - 1 && time + 1 + vec[i].x <= K; i++)
